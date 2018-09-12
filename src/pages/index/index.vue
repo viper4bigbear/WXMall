159e78119d9b6adf1fe521bf3c90849114897003
Bill <1311967697@qq.com>
2018-09-12 12:53:33 +0800

    2018年9月12日

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="header">
       <icon type="search"></icon>
-      <input type="text">
+      <input type="text" placeholder="搜索">
     </div>
   </div>
 </template>
@@ -12,6 +12,24 @@ export default {
 }
 </script>
 
-<style >
-
+<style lang="less">
+.header {
+  position: relative;
+  background-color: #ff2d4a;
+  padding: 20rpx 16rpx;
+  icon {
+    position: absolute;
+    z-index: 998;
+    left: 50%;
+    top: 30rpx;
+    transform: translateX(-200%);
+  }
+  input {
+    width: 100%;
+    height: 60rpx;
+    background-color: #fff;
+    text-align: center;
+    border-radius: 8rpx;
+  }
+}
 </style>
